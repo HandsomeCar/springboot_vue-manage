05826a60df276402c161897cbc0680d2b33e8188
car <jiaqi@ingdee.com>
2023-07-15 09:57:07 +0800

    文件夹变更

--- src/main/java/com/architecture/springboot_vuemanage/common/Config.java
+++ src/main/java/com/architecture/springboot_vuemanage/common/Config.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 前后端跨域
+ */
 @Configuration
 public class Config implements WebMvcConfigurer {
 
