bdf7c541d90d1f2c9dd6e17f0879465f3351e17f
car <jiaqi@ingdee.com>
2023-07-18 16:08:38 +0800

    后台流程更改

--- src/main/java/com/architecture/springboot_vuemanage/PersonSlt/Mapper/PersonSltMapper.java
+++ src/main/java/com/architecture/springboot_vuemanage/PersonSlt/Mapper/PersonSltMapper.java
@@ -1,7 +1,9 @@
 package com.architecture.springboot_vuemanage.PersonSlt.Mapper;
 
 import com.architecture.springboot_vuemanage.PersonSlt.Entity.PersonSltEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +11,10 @@ import java.util.List;
 
 @Mapper
 @Repository
-public interface PersonSltMapper {
+public interface PersonSltMapper extends BaseMapper<PersonSltEntity> {
 
     @Select("select * from person")
     List<PersonSltEntity> selectAll();
+
+    String getOne(@Param("id") String id);
 }
